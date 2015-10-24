343395d21dc1b49d85fa9c26f4e481d9f0e4340b
DaoNV <zendobk@gmail.com>
2015-10-24 15:26:31 +0700

    bug fix

--- SwiftUtils/Extensions/UITableView.swift
+++ SwiftUtils/Extensions/UITableView.swift
@@ -26,48 +26,36 @@ extension UITableView {
   }
   
   public func registerCellNib(aClass: AnyClass) {
-    if aClass.isSubclassOfClass(UITableViewCell.self) == false {
-      return
-    }
     let name = String(aClass)
     let nib = UINib(nibName: name, bundle: nil)
     registerNib(nib, forCellReuseIdentifier: name)
   }
   
   public func registerCellClass(aClass: AnyClass) {
-    if aClass.isSubclassOfClass(UITableViewCell.self) == false {
-      return
-    }
     let name = String(aClass)
     registerClass(aClass, forCellReuseIdentifier: name)
   }
   
   public func registerHeaderFooterNib(aClass: AnyClass) {
-    if aClass.isSubclassOfClass(UITableViewHeaderFooterView.self) == false {
-      return
-    }
     let name = String(aClass)
     let nib = UINib(nibName: name, bundle: nil)
     registerNib(nib, forHeaderFooterViewReuseIdentifier: name)
   }
   
   public func registerHeaderFooterClass(aClass: AnyClass) {
-    if aClass.isSubclassOfClass(UITableViewHeaderFooterView.self) == false {
-      return
-    }
     let name = String(aClass)
     registerClass(aClass, forHeaderFooterViewReuseIdentifier: name)
   }
   
-  public func dequeueCell<T: UITableViewCell>(aClass: T) -> T {
+  public func dequeueCell<T>(aClass: T) -> T {
     return dequeueReusableCellWithIdentifier(String(aClass)) as! T
   }
   
-  public func dequeueCell<T: UITableViewCell>(aClass: T, forIndexPath indexPath: NSIndexPath) -> T {
+  public func dequeueCell<T>(aClass: T, forIndexPath indexPath: NSIndexPath) -> T {
     return dequeueReusableCellWithIdentifier(String(aClass), forIndexPath: indexPath) as! T
   }
   
-  public func dequeueHeaderFooter<T: UITableViewHeaderFooterView>(aClass: T) -> T {
+  public func dequeueHeaderFooter<T>(aClass: T) -> T {
     return dequeueReusableHeaderFooterViewWithIdentifier(String(aClass)) as! T
   }
 }
