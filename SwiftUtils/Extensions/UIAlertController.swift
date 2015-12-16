1fe6a8e0bc9751bef0caaf084069bd35d5a70dcb
DaoNV <zendobk@gmail.com>
2015-12-16 23:38:40 +0700

    bug fix

--- SwiftUtils/Extensions/UIAlertController.swift
+++ SwiftUtils/Extensions/UIAlertController.swift
@@ -16,13 +16,19 @@ let kAppName: String = {
 }()
 
 class AlertController: UIAlertController {
-  var window: UIWindow = {
+  var window: UIWindow! = {
     let window = UIWindow(frame: UIScreen.mainScreen().bounds)
     window.windowLevel = UIWindowLevelAlert + 1
     window.rootViewController = UIViewController()
     window.makeKeyAndVisible()
     return window
   }()
+  
+  override func viewDidDisappear(animated: Bool) {
+    super.viewDidDisappear(animated)
+    window.hidden = true
+    window = nil
+  }
   
   func present() {
     window.rootViewController?.presentViewController(self, animated: true, completion: nil)
@@ -45,16 +51,14 @@ public func alert(title title: String? = kAppName, msg: String, from: UIViewCont
   return alert
 }
 
-public func alert(error: NSError?, from: UIViewController? = nil, handler:(() -> Void)? = nil) -> UIAlertController {
-  if let error = error {
-    let alert = UIAlertController(title: kAppName.localized, message: error.localizedDescription.localized, preferredStyle: UIAlertControllerStyle.Alert)
-    let ok = UIAlertAction(title: "OK".localized, style: UIAlertActionStyle.Cancel) { (action) -> Void in
-      if let handler = handler {
-        handler()
-      }
+public func alert(error: NSError, from: UIViewController? = nil, handler:(() -> Void)? = nil) -> UIAlertController {
+  let alert = AlertController(title: kAppName.localized, message: error.localizedDescription.localized, preferredStyle: UIAlertControllerStyle.Alert)
+  let ok = UIAlertAction(title: "OK".localized, style: UIAlertActionStyle.Cancel) { (action) -> Void in
+    if let handler = handler {
+      handler()
     }
-    alert.addAction(ok)
-    alert.present()
-    return alert
   }
+  alert.addAction(ok)
+  alert.present()
+  return alert
 }
